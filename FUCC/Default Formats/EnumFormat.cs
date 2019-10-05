1583fb09447bb162e8f5d4b9a18d6ceaf186990e
Felipe Martinez <pipernene@gmail.com>
2019-10-06 00:30:21 +0200

    Add EnumFormat

---
+++ FUCC/Default Formats/EnumFormat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace FUCC.DefaultFormats
+{
+    using static Expression;
+
+    internal class EnumFormat : ITypeFormat
+    {
+        public bool CanFormat(Type type) => type.IsEnum;
+
+        public Expression Deserialize(FormatContext context)
+        {
+            var enumType = context.Type.GetEnumUnderlyingType();
+            return Convert(context.Read(enumType), context.Type);
+        }
+
+        public Expression Serialize(FormatContextWithValue context)
+        {
+            var enumType = context.Type.GetEnumUnderlyingType();
+            return context.Write(enumType, Convert(context.Value, enumType));
+        }
+    }
+}
