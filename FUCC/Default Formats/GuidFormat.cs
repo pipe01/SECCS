51fae13e4ef501800ef2bc4ac054af59d160ef96
Felipe Martinez <pipernene@gmail.com>
2019-10-06 00:04:58 +0200

    Add GuidFormat

---
+++ FUCC/Default Formats/GuidFormat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace FUCC.DefaultFormats
+{
+    internal class GuidFormat : IStaticTypeFormat
+    {
+        public Type Type => typeof(Guid);
+
+        public bool CanFormat(Type type) => type == Type;
+
+        public Expression Deserialize(FormatContext context)
+        {
+            return Expression.New(
+                typeof(Guid).GetConstructor(BindingFlags.Public | BindingFlags.Instance, null, new[] { typeof(byte[]) }, null),
+                context.Read<byte[]>());
+        }
+
+        public Expression Serialize(FormatContextWithValue context)
+        {
+            return context.Write<byte[]>(Expression.Call(context.Value, nameof(Guid.ToByteArray), null));
+        }
+    }
+}
