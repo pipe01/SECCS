afb8d42b1b7f64c984f573e8c7d06b3a540b077f
Felipe Martinez <pipernene@gmail.com>
2020-03-12 16:41:33 +0100

    Add ISeccsXXX interfaces

---
+++ SECCS/Interfaces/ISeccsFormattable.cs
@@ -0,0 +1,16 @@
+﻿namespace SECCS.Interfaces
+{
+    public interface ISeccsWriteable<TWriter>
+    {
+        void Write(TWriter writer);
+    }
+
+    public interface ISeccsReadable<TReader>
+    {
+        void Read(TReader reader);
+    }
+
+    public interface ISeccsFormattable<TBuffer> : ISeccsWriteable<TBuffer>, ISeccsReadable<TBuffer>
+    {
+    }
+}
