7401419af6b2f38579de58a5606e3fdd64742160
Felipe Martinez <pipernene@gmail.com>
2020-03-12 17:27:37 +0100

    Add SeccsReadableFormat

---
+++ SECCS.Tests/Formats/SeccsReadableFormatTest.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using NUnit.Framework;
+using SECCS.Formats;
+using SECCS.Interfaces;
+using SECCS.Tests.Classes;
+using SECCS.Tests.Utils;
+
+namespace SECCS.Tests.Formats
+{
+    public class SeccsReadableFormatTest : BaseFormatTest<SeccsReadableFormat<DummyBuffer>>
+    {
+        [Test]
+        public void CanFormat_NonSeccsReadable_False()
+        {
+            Assert.IsFalse(Format.CanFormat(typeof(TestClass1)));
+        }
+
+        [Test]
+        public void CanFormat_SeccsReadable_True()
+        {
+            Assert.IsTrue(Format.CanFormat(typeof(Readable1)));
+        }
+
+        [Test]
+        public void Read_Readable_CallsReadableRead()
+        {
+            var buffer = new DummyBuffer();
+
+            Format.Read(typeof(Readable1), new ReadFormatContext<DummyBuffer>(Mock.Of<IBufferReader<DummyBuffer>>(), buffer, ""));
+
+            Assert.Fail("Read not called");
+        }
+
+        private class Readable1 : ISeccsReadable<DummyBuffer>
+        {
+            public void Read(DummyBuffer reader) => Assert.Pass();
+        }
+    }
+}
---
+++ SECCS/Formats/SeccsReadableFormat.cs
@@ -0,0 +1,20 @@
+﻿using SECCS.Interfaces;
+using System;
+
+namespace SECCS.Formats
+{
+    public class SeccsReadableFormat<TReader> : IReadFormat<TReader>
+    {
+        public bool CanFormat(Type type) => typeof(ISeccsReadable<TReader>).IsAssignableFrom(type);
+
+        public object Read(Type type, ReadFormatContext<TReader> context)
+        {
+            var obj = Activator.CreateInstance(type);
+            var readable = (ISeccsReadable<TReader>)obj;
+
+            readable.Read(context.Reader);
+
+            return obj;
+        }
+    }
+}
