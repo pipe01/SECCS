21c860a4b06440f9e056584f3b3926b2a076c147
Felipe Martinez <pipernene@gmail.com>
2020-03-13 17:36:07 +0100

    Add GuidFormat

---
+++ SECCS.Tests/Formats/MiscTypes/GuidFormat.cs
@@ -0,0 +1,44 @@
+﻿using Moq;
+using NUnit.Framework;
+using SECCS.Formats.MiscTypes;
+using SECCS.Tests.Utils;
+using System;
+using System.Linq;
+
+namespace SECCS.Tests.Formats.MiscTypes
+{
+    public class GuidFormat : BaseFormatTest<GuidFormat<DummyBuffer>>
+    {
+        [Test]
+        public void Write_Guid_CallsBufferWriter()
+        {
+            var bytes = Enumerable.Repeat((byte)0, 16).ToArray();
+
+            var buffer = new DummyBuffer();
+
+            var bufferWriterMock = new Mock<IBufferWriter<DummyBuffer>>();
+            bufferWriterMock.SetupPath(GuidFormat<DummyBuffer>.BytesPath, bytes);
+
+            var context = new WriteFormatContext<DummyBuffer>(bufferWriterMock.Object, buffer, "");
+            Format.Write(new Guid(bytes), context);
+
+            bufferWriterMock.Verify();
+        }
+
+        [Test]
+        public void Read_Guid_CallsBufferReader()
+        {
+            var buffer = new DummyBuffer();
+
+            var bufferReaderMock = new Mock<IBufferReader<DummyBuffer>>();
+            bufferReaderMock.Setup(o => o.Deserialize(buffer, typeof(byte[]), It.Is<ReadFormatContext<DummyBuffer>>(o => o.Path == "." + GuidFormat<DummyBuffer>.BytesPath)))
+                .Returns(Enumerable.Repeat((byte)0, 16).ToArray())
+                .Verifiable();
+
+            var context = new ReadFormatContext<DummyBuffer>(bufferReaderMock.Object, buffer, "");
+            Format.Read(typeof(Guid), context);
+
+            bufferReaderMock.Verify();
+        }
+    }
+}
---
+++ SECCS/Formats/Misc Types/GuidFormat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SECCS.Formats.MiscTypes
+{
+    public class GuidFormat<T> : IReadFormat<T>, IWriteFormat<T>
+    {
+        internal const string BytesPath = "GuidBytes";
+
+        public bool CanFormat(Type type) => type == typeof(Guid);
+
+        public object Read(Type type, ReadFormatContext<T> context)
+        {
+            var guidBytes = context.Read<byte[]>(BytesPath);
+
+            return new Guid(guidBytes);
+        }
+
+        public void Write(object obj, WriteFormatContext<T> context)
+        {
+            var guid = (Guid)obj;
+
+            context.Write(guid.ToByteArray(), BytesPath);
+        }
+    }
+}
