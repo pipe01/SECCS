96e7b4e5616e061cca3c28e7b3f35459ded1b23b
Felipe Martinez <pipernene@gmail.com>
2020-03-13 14:57:12 +0100

    Add extensions to FormatContext for delegate writers and readers

---
+++ SECCS/FormatCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace SECCS
+{
+    public static class FormatCollectionExtensions
+    {
+        public static void Add<T, TReader>(this FormatCollection<IReadFormat<TReader>> formats, ReadDelegate<T, TReader> reader)
+        {
+            formats.Add(new DelegateReadFormat<T, TReader>(reader));
+        }
+
+        public static void Add<T, TWriter>(this FormatCollection<IWriteFormat<TWriter>> formats, WriteDelegate<T, TWriter> writer)
+        {
+            formats.Add(new DelegateWriteFormat<T, TWriter>(writer));
+        }
+    }
+}
