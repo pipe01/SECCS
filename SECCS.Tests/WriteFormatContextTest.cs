254919050c2a7ed615d5046f88ff250b90a42169
Felipe Martinez <pipernene@gmail.com>
2020-03-13 17:55:37 +0100

    Remove usings

--- SECCS.Tests/WriteFormatContextTest.cs
+++ SECCS.Tests/WriteFormatContextTest.cs
@@ -4,10 +4,6 @@ using SECCS.Exceptions;
 using SECCS.Tests.Classes;
 using SECCS.Tests.Utils;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SECCS.Tests
 {
