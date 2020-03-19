325533e098048bae9fe9422bec6255eb6eda752a
Felipe Martinez <pipernene@gmail.com>
2020-03-19 19:56:49 +0100

    Allow SeccsMemberAttribute on methods
    This is for property accessors

--- SECCS/Attributes/SeccsMemberAttribute.cs
+++ SECCS/Attributes/SeccsMemberAttribute.cs
@@ -2,7 +2,7 @@
 
 namespace SECCS
 {
-    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Method, AllowMultiple = false)]
     public sealed class SeccsMemberAttribute : Attribute
     {
     }
