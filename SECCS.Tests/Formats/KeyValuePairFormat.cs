89f482bc1a7ab29af3f91eb53ef35dfe5111fb8d
Felipe Martinez <pipernene@gmail.com>
2020-03-19 21:50:08 +0100

    Add KeyValuePairFormat

---
+++ SECCS.Tests/Formats/KeyValuePairFormat.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using SECCS.Formats;
+using SECCS.Tests.Utils;
+using System.Collections.Generic;
+
+namespace SECCS.Tests.Formats
+{
+    internal class KeyValuePairFormat : BaseFormatTest<KeyValuePairFormat<DummyBuffer>>
+    {
+        [Test]
+        public void CanFormat_KeyValuePair_True()
+        {
+            Assert.IsTrue(Format.CanFormat(typeof(KeyValuePair<int, int>)));
+        }
+
+        [Test]
+        public void Write_KeyValuePair_CallsContext()
+        {
+            var kvp = new KeyValuePair<int, string>(123, "nice");
+
+            var contextMock = NewWriteContextMock();
+            contextMock.SetupPath("Key", kvp.Key);
+            contextMock.SetupPath("Value", kvp.Value);
+
+            Format.Write(kvp, contextMock.Object);
+
+            contextMock.Verify();
+        }
+
+        [Test]
+        public void Read_KeyValuePair_CallsContext()
+        {
+            var kvp = new KeyValuePair<int, string>(123, "nice");
+
+            var contextMock = NewReadContextMock();
+            contextMock.SetupPath("Key", kvp.Key);
+            contextMock.SetupPath("Value", kvp.Value);
+
+            var read = Format.Read(typeof(KeyValuePair<int, string>), contextMock.Object);
+
+            contextMock.Verify();
+            Assert.AreEqual(kvp, read);
+        }
+    }
+}
---
+++ SECCS/Formats/KeyValuePairFormat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SECCS.Formats
+{
+    internal class KeyValuePairFormat<T> : IReadFormat<T>, IWriteFormat<T>
+    {
+        public bool CanFormat(Type type) => type.Name == "KeyValuePair`2";
+
+        public object Read(Type type, IReadFormatContext<T> context)
+        {
+            var args = type.GetGenericArguments();
+            var keyType = args[0];
+            var valueType = args[1];
+
+            var key = context.Read(keyType, "Key");
+            var value = context.Read(valueType, "Value");
+
+            return Activator.CreateInstance(type, key, value);
+        }
+
+        public void Write(object obj, IWriteFormatContext<T> context)
+        {
+            Type t = obj.GetType();
+
+            var key = t.GetProperty("Key").GetValue(obj);
+            var value = t.GetProperty("Value").GetValue(obj);
+
+            context.Write(key, "Key");
+            context.Write(value, "Value");
+        }
+    }
+}
