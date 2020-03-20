a607b17e3dcb2ddc85a883d2825d5c1fc555bb63
Felipe Martinez <pipernene@gmail.com>
2020-03-20 23:02:22 +0100

    Add StringFormat

---
+++ SECCS/Formats/StringFormat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SECCS.Formats
+{
+    internal class StringFormat<T> : IReadFormat<T>, IWriteFormat<T>
+    {
+        public bool CanFormat(Type type, FormatOptions options) => type == typeof(string);
+
+        public object Read(Type type, IReadFormatContext<T> context)
+        {
+            return new string(context.Read<char[]>("CharArray"));
+        }
+
+        public void Write(object obj, IWriteFormatContext<T> context)
+        {
+            context.Write(((string)obj).ToCharArray(), "CharArray");
+        }
+    }
+}
